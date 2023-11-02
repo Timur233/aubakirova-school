2ef4c87821a374a5779b4c5880b5abfc06751efa
Iskandarov Timur <timur.iskandarov96@gmail.com>
2023-11-02 13:52:04 +0600

    Загрузка файлов

--- admin/controllers/self_esteem_files.php
+++ admin/controllers/self_esteem_files.php
@@ -23,7 +23,7 @@
         $file_info = pathinfo($file['name']);
         $file_extension = $file_info['extension'];
         $file_name = $request->get_param('file_name') . '-' . 
-            (microtime(true) * 1000) . '.' . $file_extension;
+            (microtime(true) * 10000) . '.' . $file_extension;
         $file_path = $upload_directory . '/' . $file_name;
 
         $response = array(
