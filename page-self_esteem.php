73033144c627ffa7012f8eaeff0600a76679060b
Iskandarov Timur <timur.iskandarov96@gmail.com>
2023-11-02 16:23:28 +0600

    Cкрипт загрузки файла

--- page-self_esteem.php
+++ page-self_esteem.php
@@ -12,19 +12,25 @@ include('widgets/breadcrumbs.php');
 
 ?>
 
-<?php
-if (post_password_required()) {
-    // Страница защищена паролем
-    echo 'Эта страница защищена паролем. Введите пароль для доступа.';
-    echo get_the_password_form(); // Выводим форму ввода пароля
-} else {
-    // Ваш код для отображения содержания страницы
-    while (have_posts()) : the_post();
-        the_title('<h1>', '</h1>');
-        the_content();
-    endwhile;
-}
-?>
+    <section class="section section-news section--gray files-page">
+        <div class="container">
+        <?php
+        if (post_password_required()) {
+            // Страница защищена паролем
+            echo 'Эта страница защищена паролем. Введите пароль для доступа.';
+            echo get_the_password_form(); // Выводим форму ввода пароля
+        } else {
+            // Ваш код для отображения содержания страницы
+            while (have_posts()) : the_post();
+                the_title('<h1>', '</h1>');
+                the_content();
+            endwhile;
+        }
+        ?>
+        </div>
+    </section>
+
+
 
 <?
 
