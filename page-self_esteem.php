0e930abe7ac3bca3cf0039faff6be6888c8f5d5c
Iskandarov Timur <timur.iskandarov96@gmail.com>
2023-11-02 10:56:35 +0600

    Самооценка  - создал шаблон страницы

---
+++ page-self_esteem.php
@@ -0,0 +1,21 @@
+<?php
+/*
+    Template Name: Самооценка, страница для служб
+    Template Post Type: post, page, product
+*/
+
+get_header(null, array(
+    'header_color_class' => 'main-header--white'
+));
+
+include('widgets/breadcrumbs.php');
+
+?>
+
+
+
+<?
+
+get_footer();
+
+?>
