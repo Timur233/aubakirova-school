855f1109bc5ea039a0d64d2b911f914e208d9ffe
Iskandarov Timur <timur.iskandarov96@gmail.com>
2023-11-02 11:34:52 +0600

    Новые правки

--- admin/admin.php
+++ admin/admin.php
@@ -17,6 +17,17 @@ function self_esteem_metabox() {
 
 add_action('add_meta_boxes', 'self_esteem_metabox');
 
+function hide_editor_and_thumbnail_self_esteem() {
+	global $post;
+
+	if ($post && get_page_template_slug($post) === 'page-self_esteem.php') {
+		remove_post_type_support('post', 'editor');
+		remove_post_type_support('post', 'thumbnail');
+	}
+}
+
+add_action('admin_init', 'hide_editor_and_thumbnail_self_esteem');
+
 /**
  * Кастомные стили адми панели
  */
