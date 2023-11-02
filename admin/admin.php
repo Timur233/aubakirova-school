12b9a674ee5a40697ea1cce3b6559864e13a9357
Iskandarov Timur <timur.iskandarov96@gmail.com>
2023-11-02 11:35:40 +0600

    Новые правки

--- admin/admin.php
+++ admin/admin.php
@@ -21,12 +21,12 @@ function hide_editor_and_thumbnail_self_esteem() {
 	global $post;
 
 	if ($post && get_page_template_slug($post) === 'page-self_esteem.php') {
-		remove_post_type_support('post', 'editor');
-		remove_post_type_support('post', 'thumbnail');
+		remove_post_type_support('page', 'editor');
+		remove_post_type_support('page', 'thumbnail');
 	}
 }
 
-add_action('admin_init', 'hide_editor_and_thumbnail_self_esteem');
+add_action('init', 'hide_editor_and_thumbnail_self_esteem');
 
 /**
  * Кастомные стили адми панели
